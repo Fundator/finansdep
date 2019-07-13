47e4906e064172eb6f354d22555b0585e53c7acc
thomasht86 <thomas.h.thoresen@gmail.com>
2019-07-13 05:23:16 +0200

    working deployment

--- train.py
+++ train.py
@@ -1,59 +1,81 @@
 import pandas as pd
 import numpy as np
-import string
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report, f1_score, auc as sk_auc, roc_curve, precision_score, recall_score, accuracy_score
-from sklearn.pipeline import make_pipeline
-from sklearn.decomposition import TruncatedSVD
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.naive_bayes import MultinomialNB
-import pickle
-import random
-import re
-#import keras
-import os
-import argparse
+from catboost import CatBoostRegressor, Pool
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import mlflow
 from mlflow import log_metric, log_param, log_artifact
 import mlflow.sklearn
 import mlflow.pyfunc
-import matplotlib.pyplot as plt 
-import seaborn as sns 
+import json
+import shap
+import argparse
 
-class TextClassifier(mlflow.pyfunc.PythonModel):
+categorical_feature_names = ["Suburb", "Type", "Method", "Regionname", "CouncilArea"]
+columns_to_encode = ["Suburb", "Type", "Method", "Postcode", "Regionname", "CouncilArea"]
+columns_to_drop = ["Address", "Date", "Price", "SellerG", "LogPrice"]
 
-    def __init__(self, vectorizer, clf):
-        self.vectorizer = vectorizer
-        self.clf = clf
+class HousePricePredictor(mlflow.pyfunc.PythonModel):
+
+    def __init__(self, encoder, explainer, cat_col_idx, reg):
+        self.encoder = encoder
+        self.reg = reg
+        self.explainer = explainer
+        self.cat_col_idx = cat_col_idx
         
     def predict(self, context, model_input):
         print(model_input)
         print(type(model_input))
-        prepped_input = self.vectorizer.transform(model_input["description"])
-        print(prepped_input.shape)
-        preds = self.clf.predict_proba(prepped_input)
-        resp = []
-        for pred in preds:
-            pred_resp = {c:p for c, p in zip(clf.classes_, pred)}
-            resp.append(pred_resp)
-        return resp
+        df = model_input
+        df[categorical_feature_names] = df[categorical_feature_names].astype("category")
+        df[columns_to_encode] = self.encoder.transform(df[columns_to_encode])
+        df["Month"] = df["Date"].dt.month
+        df["Year"] = df["Date"].dt.year 
+        df = df.drop("Date", axis=1)
+        print(df)
+        pred = self.reg.predict(df)
+        shap_values = explainer.shap_values(Pool(df, pred, cat_features=cat_col_idx))
+        print(shap_values)
+        resp = {}
+        resp["Prediction"] = pred
+        resp["Explanation"] = {name:value for name, value in zip(df.columns, shap_values)}
+        return json.dumps(resp)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--seed", help="Specify seed for reproducibility", type=int, default=42)
-parser.add_argument("-i", "--idf", help="Whether to use inverse document frequency", type=bool, default=True)
-parser.add_argument("-n", "--ngrams", help="Number of character sequences to use as features", type=int,
-                    choices=[1,2,3], default=1)
-parser.add_argument("-u", "--use_stoplist", help="Whether to remove most common words", type=bool, default=True)
-parser.add_argument("-d", "--datafile", help="Path to datafile", type=str, default="mlflow/data/course_descriptions.csv")
-parser.add_argument("-m", "--model", help="Model type", type=str, choices=["nb", "log_reg"], default="nb")
-# Naive Bayes args
-parser.add_argument("-a", "--alpha", help="Naive bayes smoothing parameter (0=no smoothing)", type=float, default=1.)
-parser.add_argument("-f", "--fit_prior", help="Whether to fit priors", type=bool, default=True)
-# Logreg args 
-parser.add_argument("-C", "--C", help="Logistic regression regularization parameter", type=float, default=1.)
+parser.add_argument("-d", "--datafile", help="Path to datafile", type=str, default="MELBOURNE_HOUSE_PRICES_LESS.csv")
 
+def winsorized_absolute_log_error(y_true, y_pred, epsilon=1e-7):
+        return np.mean(np.minimum(np.abs(np.log(y_pred+epsilon) - np.log(y_true+epsilon)), 0.4))
+
+def get_metrics(true, pred, method="unknown", print_metrics=True, log_target=True):
+    """
+    Get the four metrics specified.
+    true: Ground true data
+    pred: Predicted data
+    method: Type of model
+    print_metrics: If metrics should be printer, default=True
+    log_target: If the target value is logaritmic, we should do an inverse transform to get metrics in original units, default=True
+    """
+    if log_target:
+        true, pred = np.exp(true), np.exp(pred)
+
+    WALE = winsorized_absolute_log_error(true,pred)
+    RMSE = np.sqrt(mean_squared_error(true, pred))
+    MAE = mean_absolute_error(true, pred)
+    R2 = r2_score(true, pred)
+    return_dict = {"WALE": WALE, "RMSE": RMSE, "MAE": MAE, "R2": R2}
+    if print_metrics:
+        for k,v in return_dict.items():
+            print(f"{k}: {v}")
+    return return_dict
+
+def split_dataset(dataframe, val_index, holdout_test_index):
+    train = dataframe.iloc[:val_index]
+    val = dataframe.iloc[val_index:holdout_test_index]
+    holdout_test = dataframe.iloc[holdout_test_index:]
+    return train, val, holdout_test
 
 if __name__ == "__main__":
 
@@ -61,118 +83,61 @@ if __name__ == "__main__":
     print("Using arguments:")
     print(args)
     seed = args.seed
-    use_idf = args.idf
-    ngram_range = (1,args.ngrams)
-    alpha = args.alpha
-    fit_prior = args.fit_prior
-    use_stoplist = args.use_stoplist
-    use_svd = False
-    C = args.C
-    model_dict = {"nb":MultinomialNB(alpha=alpha, fit_prior=fit_prior),
-                  "log_reg": LogisticRegression(C=C, multi_class="ovr", solver="saga", max_iter=1000)}
-    clf = model_dict[args.model]
-
-    conda_env_path = "condaenv.yml"
+    datafile = args.datafile
+    
+    conda_env_path = "environment.yml"
     model_prefix = "/models"
     data_file = args.datafile
-
-    #mlflow.set_experiment("ntnu_course_classifier")
 
     with mlflow.start_run() as run:
         # Get path to save model
         tracking_uri = mlflow.tracking.get_tracking_uri() 
         print("Logging to "+tracking_uri)
-        artifact_uri = mlflow.get_artifact_uri().split("file://")[1]
+        artifact_uri = mlflow.get_artifact_uri()
         print("Saving artifacts to "+artifact_uri)
         model_path = artifact_uri+model_prefix
-
-        # Log params
         mlflow.log_param("seed", seed)
-        mlflow.log_param("use_idf", use_idf)
-        mlflow.log_param("use_stoplist", use_stoplist)
-        mlflow.log_param("ngrams", args.ngrams)
         mlflow.log_param("data_file", data_file)
-        mlflow.log_param("model_type", args.model)
-        #if args.model == "nb":
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("fit_prior", fit_prior)
-        #if args.model == "log_reg":
-        mlflow.log_param("C", C)
-
-        ## Data import and cleaning
-        df = pd.read_csv(data_file, usecols=[1,2,3,4,5,6])
-        df = df.dropna()
-
-        def remove_punctuation(document):
-            # Remove weird characters
-            return "".join([ (c if c not in string.punctuation+"\n\r\t" else " ") for c in document])
-
-        def tokenize(document):
-            # From document to list of lower case tokens
-            return [w.lower() for w in remove_punctuation(document).split(" ") if len(w)>0]
-
-        if use_stoplist:
-            stoplist = [l.strip() for l in open("stopwords.txt", "r").readlines()]
-        else:
-            stoplist = None
-
-        # Create pandas Series with target variable 
-        y = df["fac"].astype(str)
-
-        # Split into training and test set
-        X_train, X_test, y_train, y_test = train_test_split(df["description"], y, stratify=y, random_state=seed)
-
-        # Initialize the TfidfVectorizer
-        vec = TfidfVectorizer(tokenizer=tokenize, stop_words=stoplist, use_idf=use_idf, ngram_range=ngram_range)
         
-        if use_svd:
-            svd = TruncatedSVD(n_components=100, n_iter=50)
-            scaler = MinMaxScaler()
-            vec = make_pipeline(vec, svd, scaler)
+        df = pd.read_csv(datafile, parse_dates=["Date"])
+        df = df.dropna(subset=["Price"])
 
-        # Fit to training set and transform test set
-        trn_vec= vec.fit_transform(X_train.values)
-        test_vec = vec.transform(X_test.values)
+        df[categorical_feature_names] = df[categorical_feature_names].astype("category")
+        df["Month"] = df["Date"].dt.month
+        df["Year"] = df["Date"].dt.year
 
-        # Create a list of the unique target values
-        label_cols = df["fac"].astype(str).unique().tolist()
+        df["LogPrice"] = df["Price"].apply(np.log)
 
-        # Fit the model
-        clf.fit(trn_vec, y_train)
+        df = df.sort_values(by="Date")
+        holdout_test_idx = df.shape[0] - int(df.shape[0] * 0.1)
+        val_idx = df.shape[0] - int(df.shape[0] * 0.1) * 2
 
-        # Make predictions with test set
-        preds = clf.predict(test_vec)
+        rfc_enc = OrdinalEncoder()
+        rfc_df = df
+        rfc_df[columns_to_encode] = rfc_enc.fit_transform(df[columns_to_encode])
 
-        textclf = TextClassifier(vec, clf)
+        rfc_train, rfc_valid, rfc_holdout_test = split_dataset(rfc_df, val_idx, holdout_test_idx)
+
+        rfc_X_train = rfc_train.drop(columns=columns_to_drop)
+        rfc_y_train = rfc_train["LogPrice"]
+
+        rfc_X_valid = rfc_valid.drop(columns=columns_to_drop)
+        rfc_y_valid = rfc_valid["LogPrice"]
+
+        rfc_X_test = rfc_holdout_test.drop(columns=columns_to_drop)
+        rfc_y_test = rfc_holdout_test["LogPrice"]
+
+        cat_col_idx = np.array([rfc_X_train.columns.get_loc(c) for c in columns_to_encode])
+        rfc = CatBoostRegressor(iterations=500, loss_function="RMSE", task_type="CPU", border_count=256, model_size_reg=0)
+        rfc.fit(X=rfc_X_train, y=rfc_y_train, cat_features=cat_col_idx, eval_set=(rfc_X_valid, rfc_y_valid), silent=True, plot=True)
+        explainer = shap.TreeExplainer(rfc)
+        
+        housereg = HousePricePredictor(rfc_enc, explainer, cat_col_idx, rfc)
         print("Saving model to "+model_path)
-        mlflow.pyfunc.save_model(model_path, conda_env=conda_env_path, python_model=textclf)
+        mlflow.pyfunc.save_model(model_path, conda_env=conda_env_path, python_model=housereg)
         
-        # Calculate metrics from predictions and y_test
-        acc = accuracy_score(y_test, preds)
-        f1_macro = precision_score(y_test, preds, average="macro")
-        f1_micro = precision_score(y_test, preds, average="micro")
-        f1_per_class = precision_score(y_test, preds, labels=label_cols, average=None)
-        cm = confusion_matrix(y_test, preds, labels=label_cols)
-        df_cm = pd.DataFrame(cm, index=label_cols, columns=label_cols)
-        cm_plot = sns.heatmap(df_cm, annot=True, fmt="d")
-        heatmap_path = artifact_uri+"/cm_heatmap.png"
-        fig = cm_plot.get_figure()
-        fig.savefig(heatmap_path)
+        rfc_pred = rfc.predict(rfc_X_test)
 
-        mlflow.log_metric("accuracy", acc)
-        mlflow.log_metric("f1_macro", f1_macro)
-        mlflow.log_metric("f1_micro", f1_micro)
-        for c, f1 in zip(label_cols, f1_per_class):
-            mlflow.log_metric("f1_"+c, f1)        
-        #mlflow.sklearn.save_model(clf, model_path, conda_env=conda_env_path, serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE)
-
-        #%% [markdown]
-        # ## Next steps
-        # - Define metrics
-        # - Plot
-        # - Script evolution
-        # - Think about steps
-
-
-
-
+        rfc_metrics = get_metrics(rfc_y_test, rfc_pred)
+        for metric_name, metric_value in rfc_metrics.items():
+            mlflow.log_metric(metric_name, metric_value) 
